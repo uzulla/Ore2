92853e949b10fe87a46add5efedee858738b073d
uzulla <uzulla@himitsukichi.com>
2016-03-31 19:13:47 +0900

    refactor

--- lib/Ore2/Template.php
+++ lib/Ore2/Template.php
@@ -1,91 +1,64 @@
 <?php
 namespace Ore2;
 
-/* sample
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    {{ name }}
-
-    {% for item in list %}
-        {{ item }}
-    {% endfor %}
-</body>
-</html>
-*/
-
 class Template
 {
-    public $template_dir = __DIR__."/template";
+    public $template_dir = __DIR__ . "/template";
 
     public function __construct($config)
     {
-        if(isset($config['template_dir']))
+        if (isset($config['template_dir']))
             $this->template_dir = $config['template_dir'];
     }
 
-    public function parse($file_name){
+    public function parse($file_name)
+    {
         $file_path = $this->template_dir . '/' . $file_name;
         $template = file_get_contents($file_path);
 
         $chars = preg_split('//u', $template, -1, PREG_SPLIT_NO_EMPTY);// $chars[0]; でマルチバイトあつかいたいね…
         $length = count($chars);
 
-        $buffer = '';
+        $code = '';
         for ($i = 0; $length > $i; $i++) {
+            if ($chars[$i] === "{" && ($chars[$i + 1] === "{" || $chars[$i + 1] === "%")) {
+                // {{ あるいは {% をひろう
+                $is_print = ($chars[$i + 1] === "{"); // "{{" はprint
+                $i = $i + 2;
 
-            if ($chars[$i] === "{" && $chars[$i + 1] === "{") {
-                $i++;
-                $i++;
-                $expression_buffer = '';
+                $token_buffer = '';
                 for (; $length > $i; $i++) {
-                    if ($chars[$i] == "}" && $chars[$i + 1] == "}") {
+                    if (($chars[$i] == "}" || $chars[$i] == "%") && $chars[$i + 1] == "}") {
+                        // }} あるいは %} なら token 読み取りを終了
                         echo PHP_EOL;
                         $i++;
                         break;
                     }
-                    $expression_buffer .= $chars[$i];
+                    $token_buffer .= $chars[$i];
                 }
-                $buffer .= '<?php echo htmlspecialchars($' . trim($expression_buffer) . ',ENT_QUOTES, "UTF-8"); ?>';
-                continue;
 
-            } else if ($chars[$i] === "{" && $chars[$i + 1] === "%") {
-                $i++;
-                $i++;
-                $expression_buffer = '';
-                for (; $length > $i; $i++) {
-                    if ($chars[$i] == "%" && $chars[$i + 1] == "}") {
-                        echo PHP_EOL;
-                        $i++;
-                        break;
+                if ($is_print) {
+                    $code .= '<?php echo htmlspecialchars($' . trim($token_buffer) . ',ENT_QUOTES, "UTF-8"); ?>';
+                } else {
+                    $tokens = preg_split('/\s+/u', trim($token_buffer), -1, PREG_SPLIT_NO_EMPTY);
+                    if ($tokens[0] === 'for') {
+                        $code .= '<?php foreach( $' . $tokens[3] . ' as $' . $tokens[1] . ' ){ ?>';
+                    } else if ($tokens[0] === 'endfor') {
+                        $code .= '<?php } ?>';
                     }
-                    $expression_buffer .= $chars[$i];
                 }
-                $tokens = preg_split('/\s+/u', trim($expression_buffer), -1, PREG_SPLIT_NO_EMPTY);
-                if ($tokens[0] === 'for') {
-                    $buffer .= '<?php foreach( $' . $tokens[3] . ' as $' . $tokens[1] . ' ){ ?>';
-                } else if ($tokens[0] === 'endfor') {
-                    $buffer .= '<?php } ?>';
-                }
-                continue;
             } else {
-                $buffer .= $chars[$i];
+                $code .= $chars[$i];
             }
         }
-        return $buffer;
+        return $code;
     }
 
-    public function execute($buffer, $params){
-//        echo $buffer . PHP_EOL;
+    public function execute($buffer, $params)
+    {
         $run = function ($code, $params) {
-            foreach ($params as $key => $param) {
-                $$key = $param;
-            }
-            eval('?>' . $code);
+            foreach ($params as $key => $param) $$key = $param;
+            eval('?>' . $code); // evalは最初からPHPコードを要求するので、最初に終わらせる
         };
 
         ob_start();
