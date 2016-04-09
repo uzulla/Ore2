ed204ff69d2aa4b5b9131a1dc1bc0e9cbc362b6d
uzulla <uzulla@himitsukichi.com>
2016-04-09 23:56:53 +0900

    chore

--- lib/Ore2/Router.php
+++ lib/Ore2/Router.php
@@ -3,13 +3,15 @@ namespace Ore2;
 
 use Psr\Http\Message\RequestInterface;
 
-class RouteNotFoundException extends \Exception{}
+class RouteNotFoundException extends \Exception
+{
+}
 
 class Router
 {
     public $route = [
-        "post"=>[],
-        "get"=>[]
+        "post" => [],
+        "get" => []
     ];
 
     public $container;
@@ -19,26 +21,26 @@ class Router
         $this->container = $container;
     }
 
-    public function any(string $path, callable $callback)
+    public function any(string $path, $callback)
     {
-        foreach(array_keys($this->route) as $method)
+        foreach (array_keys($this->route) as $method)
             $this->route[$method][$path] = $callback;
     }
 
-    public function get(string $path, callable $callback)
+    public function get(string $path, $callback)
     {
         $this->setRoute('get', $path, $callback);
     }
 
-    public function post(string $path, callable $callback)
+    public function post(string $path, $callback)
     {
         $this->setRoute('post', $path, $callback);
     }
 
-    public function setRoute(string $method, string $path, callable $callback)
+    public function setRoute(string $method, string $path, $callback)
     {
         $method = strtolower($method);
-        if(!isset($this->route[$method]))
+        if (!isset($this->route[$method]))
             throw new \InvalidArgumentException('Not acceptable method');
 
         $this->route[$method][$path] = $callback;
@@ -49,11 +51,12 @@ class Router
      * @return RouterResult
      * @throws RouteNotFoundException
      */
-    public function run(RequestInterface $request){
+    public function run(RequestInterface $request):RouterResult
+    {
         $method = strtolower($request->getMethod());
         $uri = $request->getRequestTarget();
 
-        if(!isset($this->route[$method]))
+        if (!isset($this->route[$method]))
             throw new \InvalidArgumentException('Not acceptable method');
 
         $route_list = $this->route[$method];
@@ -63,14 +66,16 @@ class Router
         foreach ($route_list as $route => $cb) {
             $regex_list[$route] = preg_replace_callback(
                 '#:([\w]+)#',
-                function ($m) { return "(?P<{$m[1]}>[^/]+)"; },
+                function ($m) {
+                    return "(?P<{$m[1]}>[^/]+)";
+                },
                 $route
             );
         }
 
         // do match!
         $matches = [];
-        $match_route =false;
+        $match_route = false;
 
         foreach ($regex_list as $_path => $regex) {
             if (preg_match("#\A{$regex}\z#u", $uri, $matches)) {
@@ -83,11 +88,12 @@ class Router
 
 
         // response callback
-        $params = preg_grep ('/[0-9]/u', $matches, PREG_GREP_INVERT);
-        array_walk($params, function(&$v){$v = urldecode($v);});
+        $params = preg_grep('/[0-9]/u', $matches, PREG_GREP_INVERT);
+        array_walk($params, function (&$v) {
+            $v = urldecode($v);
+        });
 
         $cb = $this->route[$method][$match_route];
-
 
         return new RouterResult($this->container, $cb, $params);
     }
@@ -103,19 +109,22 @@ class RouterResult
     public $container;
     public $action;
 
-    public function __construct(Container $container, callable $action, array $params)
+    public function __construct(Container $container, $action, array $params)
     {
         $this->container = $container;
         $this->container->routeParams = $params;
         $this->action = $action;
     }
 
-    public function __invoke($request, $response){
+    public function __invoke($request, $response)
+    {
         $action = $this->action;
 
-        if($this->action instanceof \Closure){
+        if ($this->action instanceof \Closure) {
             $action = $action->bindTo(new Action($this->container, $request, $response));
-        }else{
+        } else if ($this->action instanceof Action) {
+            // ok
+        } else {
             // クラス名で起動とかやりたいよね
             throw new \InvalidArgumentException('not implemented yet');
         }
