1aa8e65c7d32b66a2dcc7622709c7f70591db94f
uzulla <uzulla@himitsukichi.com>
2016-04-10 04:13:02 +0900

    refactor

--- lib/Ore2/Router/MatchAction.php
+++ lib/Ore2/Router/MatchAction.php
@@ -3,6 +3,7 @@ namespace Ore2\Router;
 
 use Ore2\Action;
 use Ore2\Container;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class MatchAction
@@ -17,18 +18,16 @@ class MatchAction
         $this->action = $action;
     }
 
-    public function __invoke($request, $response):ResponseInterface
+    public function __invoke(RequestInterface $request, ResponseInterface $response):ResponseInterface
     {
         $action = $this->action;
         $methodName = "__invoke";
 
         if ($action instanceof \Closure) {
             $action = $action->bindTo(new Action($this->container, $request, $response));
-
         } else {
-            if (!preg_match('|\A([\\a-zA-Z0-9_]*)::([a-zA-Z0-9_]+)\z|u', $action, $matches)) {
+            if (!preg_match('|\A([\\a-zA-Z0-9_]*)::([a-zA-Z0-9_]+)\z|u', $action, $matches))
                 throw new \InvalidArgumentException('invalid action string:' . $action);
-            }
 
             $action = new $matches[1]($this->container, $request, $response);
             $methodName = $matches[2];
@@ -36,5 +35,4 @@ class MatchAction
 
         return $action->$methodName($request, $response);
     }
-
 }
