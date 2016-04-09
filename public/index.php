88734cadd2065867292d0b216295fa3e7724d620
uzulla <uzulla@himitsukichi.com>
2016-04-10 03:07:15 +0900

    試行錯誤

--- public/index.php
+++ public/index.php
@@ -1,33 +1,33 @@
 <?php
 include __DIR__."/../vendor/autoload.php";
 
+// 各種ヘルパーをコンテナにいれる
 $container = new \Ore2\Container();
 $container->config = require __DIR__."/../settings.php";
 $container->session = new \Ore2\Session(new \Ore2\Session\Storage\PHPSession());
 $container->template = new \Ore2\Template($container->config['template']);
 $container->logger = new \Ore2\Logger();
-// $container = new Orm
 
-$request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
-$response = new \Zend\Diactoros\Response();
-
+// Route 登録
 $router = new \Ore2\Router($container);
-
 $router->get('/', function(){
     return $this->html('<span style="color:red">123</span>');
 });
-
 $router->get('/name/:name', function(){
     return $this->html("hello {$this->c->routeParams['name']}");
 });
-
 $router->get('/sample', '\\MyApp\\SampleAction::sample');
 $router->get('/sample_json', '\\MyApp\\SampleAction::sampleJson');
 
-/** @var \Ore2\Router\MatchAction $action */
+// PSR-7オブジェクトのファクトリを実装するの諦めました
+$request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+$response = new \Zend\Diactoros\Response();
+
+// route 解決
 $action = $router->findMatch($request->getMethod(), $request->getRequestTarget());
 
-// if you need middleware, insert here.
+// 実行
 $response = $action($request, $response);
 
+// 送信
 \Ore2\Transmitter::sendResponse($response);
