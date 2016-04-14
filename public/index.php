9e9c17cfe2e1b391a50ad7c297342d0f73a357fa
uzulla <uzulla@himitsukichi.com>
2016-04-14 11:05:24 +0900

    お化粧

--- public/index.php
+++ public/index.php
@@ -1,41 +1,37 @@
 <?php
-declare(strict_types=1);
-include __DIR__."/../vendor/autoload.php";
+declare(strict_types = 1);
+include __DIR__ . "/../vendor/autoload.php";
 
 // Route 登録
 $router = new \Ore2\Router();
-
-$router->get('/',            '\\MyApp\\Action\\Post::showList');
-$router->get('/post/:id',    '\\MyApp\\Action\\Post::show');
+$router->get('/', '\\MyApp\\Action\\Post::showList');
+$router->get('/post/:id', '\\MyApp\\Action\\Post::show');
 $router->post('/post/create', '\\MyApp\\Action\\Post::create');
 
-$router->get('/json_sample', function(){
-    return $this->json(['this'=>'is', 'json'=>'data']);
-});
-
-// 各種ヘルパーをコンテナにいれる
+// 各種をコンテナにいれる
 $container = new \Ore2\Container();
-$container->config = require __DIR__."/../settings.php";
+$container->config = require __DIR__ . "/../settings.php";
 $container->router = $router;
 $container->session = new \Ore2\Session(new \Ore2\Session\Storage\PHPSession());
 $container->logger = new \Ore2\Logger();
+$container->viewParams = [];
 $container->template =
     new Twig_Environment(
         new Twig_Loader_Filesystem($container->config['template']['template_dir']),
         ['debug' => true]
     );
-$container->viewParams = [];
 \MyApp\Db::$pdo = new \PDO($container->config['db']['dsn']);
 
-// PSR-7オブジェクトのファクトリを実装するの諦めました
+// PSR-7インスタンスを自前実装するの諦めました
 $request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
 $response = new \Zend\Diactoros\Response();
-// route 解決
-$match_result = $router->findMatch($request->getMethod(), $request->getRequestTarget());
-// アクションを作成
-$action = $match_result->buildAction($container);
 
-// Middlewareとして実行
+// アクションを作成
+$action = $router
+    ->findMatchByRequest($request)
+    ->buildAction($container);
+
+// Middlewareのスタックを定義
 $seqencer = new \Ore2\MiddlewareSequencer([
     new \Ore2\Transmitter(),
     new \Ore2\ExceptionCatcher(),
@@ -44,4 +40,5 @@ $seqencer = new \Ore2\MiddlewareSequencer([
     $action
 ]);
 
+// Middlewareとして実行
 $seqencer($request, $response);
