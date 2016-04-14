166534a835cfba86d25d92075efa856d5918bfdc
uzulla <uzulla@himitsukichi.com>
2016-04-14 19:54:01 +0900

    勢いでPSR-11に対応
    https://github.com/php-fig/fig-standards/blob/master/proposed/container.md

--- lib/Ore2/Container.php
+++ lib/Ore2/Container.php
@@ -2,6 +2,10 @@
 declare(strict_types=1);
 namespace Ore2;
 
+use Psr\Container\ContainerInterface;
+use Psr\Container\Exception\ContainerExceptionInterface;
+use Psr\Container\Exception\NotFoundExceptionInterface;
+
 /**
  * 素朴なオブジェクトコンテナ。setされたオブジェクトがClosureなら読み込み時に実行する
  * arrayと、__get/setアクセスに対応
@@ -9,7 +13,7 @@ namespace Ore2;
  * Class Container
  * @package Ore2
  */
-class Container implements \ArrayAccess
+class Container implements \ArrayAccess, ContainerInterface
 {
     /** @var array シングルトンで取得できるコンテナを保存しておく配列 */
     static $instanceList = [];
@@ -71,4 +75,49 @@ class Container implements \ArrayAccess
             unset($this->list[$offset]);
         }
     }
+
+    /*
+     * PSR-11(Draft) implements
+     * https://github.com/php-fig/fig-standards/blob/master/proposed/container.md
+     */
+
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @throws NotFoundExceptionInterface  No entry was found for this identifier.
+     * @throws ContainerExceptionInterface Error while retrieving the entry.
+     *
+     * @return mixed Entry.
+     */
+    public function get($id)
+    {
+        if (!$this->has($id)) {
+            throw new NotFoundException('Not Found');
+        }
+
+        return $this->__get($id);
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return boolean
+     */
+    public function has($id)
+    {
+        return $this->offsetExists($id);
+    }
+}
+
+class NotFoundException extends \Exception implements NotFoundExceptionInterface
+{
+}
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
 }
---
+++ lib/Psr/Container/Exception/NotFoundExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace Psr\Container\Exception;
+
+/**
+ * No entry was found in the container.
+ */
+interface NotFoundExceptionInterface extends ContainerExceptionInterface
+{
+}
